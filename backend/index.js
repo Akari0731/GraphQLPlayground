8a8bc5396094f6d43a5c22291b0e6e796444f736
Akari Nishii <nishii0731@gmail.com>
2021-08-22 13:18:03 +0200

    Change port and add cors

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 import { port } from './config/index.js';
 import app from './app';
 import connectDB from './db';
+import cors from 'cors';
 
 const start = async () => {
   try {
     console.log('Connecting to database');
     await connectDB();
     console.log('Connected to database');
+
+     app.use(cors())
 
     await app.listen(port);
     console.log(`🚀  GraphQL server running at port: ${port}`);
